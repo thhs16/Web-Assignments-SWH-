6cf48961a83ccd8415380cdaacab5700b0c5756a
thhs16 <thaehtapehtarsan@gmail.com>
2023-09-19 21:34:57 +0630

    ranNumGenerator

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// server
+ const express = require('express')
+ const app = express()
+ const port = 3000
+
+ const path = require('path')
+
+ app.get('/',(req, res) => {
+          res.sendFile(path.join(__dirname, "index.html"))
+ })
+ 
+ app.listen(port, () =>{
+          console.log('Server is starting at port 3000')
+ })
