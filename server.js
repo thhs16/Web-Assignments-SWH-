e77612fb28a55919bdb539b535ee3616e65f4bff
thhs16 <thaehtapehtarsan@gmail.com>
2023-09-19 22:23:43 +0630

    random number generator

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 
 // server
- const express = require('express')
- const app = express()
- const port = 3000
+ const express = require('express');
+ const app = express();
+ const port = 3000;
 
- const path = require('path')
+ const path = require('path');
+
+ const publicFold = path.join(__dirname, 'public');
+ app.use(express.static(publicFold)); // to serve static files from the "public" directory
 
  app.get('/',(req, res) => {
-          res.sendFile(path.join(__dirname, "index.html"))
- })
+          res.sendFile(path.join(__dirname, "index.html"));
+ });
  
  app.listen(port, () =>{
           console.log('Server is starting at port 3000')
- })
+ });
